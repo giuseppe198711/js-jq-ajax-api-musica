c080d287544d1c2ac9a1805fefca06b9641c5a2c
Giuseppe <giuseppedf8711@gmail.com>
2020-09-19 00:06:44 +0200

    inserito anche la select

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ $(document).ready (function() {
        "url": "https://flynn.boolean.careers/exercises/api/array/music",
        "method": "GET",
        "success": function (data, status) {
+         // data è un oggetto che contiene un array di un album e lo stato della risposta
+         //
          var result = data.response;
+         // result è l array di album e non tutto l oggetto
+         console.log(data);
 
          render(result);
        },
@@ -24,6 +28,21 @@ $(document).ready (function() {
 
 
 
+
+$(".genere").change(function() {
+  var valueSelect = $(this).val();
+
+  if (valueSelect == "") {
+    $(".cd").show();
+  } else {
+    $(".cd").hide();
+    $(".cd[data-genere='"+valueSelect+"']").show();
+  }
+
+});
+
+
+
 function render (results) {
   var source = $("#disco-template").html();
   var template = Handlebars.compile(source);
@@ -31,15 +50,22 @@ function render (results) {
   for (var i = 0; i < results.length; i++) {
     var album = results[i];
 
+  // in alternativa  se i nomi all'interno dell'oggetto della risposta non corrispondono a
+  // quelli utilizzati nel template ci creiamo un nuovo oggetto contex che contiene
+  // le proprietà utilizzate nel nostro teamplate.
 
-    var context = {
-      "poster": album.poster,
-      "title": album.title,
-      "author": album.author,
-      "year": album.year
-    };
+    // console.log(context);
+    // var context = {
+    //
+    //   "poster": album.poster,
+    //   "title": album.title,
+    //   "author": album.author,
+    //   "year": album.year,
+    //   "genre": album.genre,
+    // };
+    
 
-    var html = template(context);
+    var html = template(album);
 
     $(".cds-container").append(html);
   }
